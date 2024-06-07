935f98e5534feb7326d474ed131c8d75d3ddd5c3
damladatabase <damla.database@gmail.com>
2024-06-07 10:29:51 +0500

    static

--- delivery_control/urls.py
+++ delivery_control/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('control.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 admin.site.index_title = 'Delivery Control '
 admin.site.site_title = 'Delivery Control'
